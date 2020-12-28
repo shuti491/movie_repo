5e2cc7bfda98577e8d378f54857c8c9f8e7abeae
shuti491 <68851931+shuti491@users.noreply.github.com>
2020-12-28 11:09:10 +0530

    Update RouteDel.js

--- src/RouteDel.js
+++ src/RouteDel.js
@@ -6,14 +6,7 @@ import {
   Link
 } from "react-router-dom";
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
+
 
 export default function BasicExample() {
   return (
@@ -33,13 +26,7 @@ export default function BasicExample() {
 
         <hr />
 
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
+       
         <Switch>
           <Route exact path="/">
             <Home />
@@ -56,8 +43,6 @@ export default function BasicExample() {
   );
 }
 
-// You can think of these components as "pages"
-// in your app.
 
 function Home() {
   return (
